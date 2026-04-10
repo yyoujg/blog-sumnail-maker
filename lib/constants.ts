7eeb8ae91e21de77e4df0e36b21666df926dd8fd
yyoujg <andn1026@gmail.com>
2026-04-10 18:45:12 +0900

    폰트 눈누 인기 폰트로 교체: 나눔스퀘어라운드, GmarketSans, 카페24써라운드, 에스코어드림

--- lib/constants.ts
+++ lib/constants.ts
@@ -7,21 +7,20 @@ export const ADSENSE_SCRIPT_SRC = `https://pagead2.googlesyndication.com/pagead/
 export const ADSENSE_AD_SLOT = '9150939859';
 
 export const FONT_LINK_HREF =
-  'https://fonts.googleapis.com/css2?family=Black+Han+Sans&family=Do+Hyeon&family=Gothic+A1:wght@700;900&family=Gugi&family=Jua&family=Nanum+Gothic:wght@700&family=Noto+Sans+KR:wght@700;900&family=Single+Day&family=Sunflower:wght@700&display=swap';
+  'https://fonts.googleapis.com/css2?family=Black+Han+Sans&family=Do+Hyeon&family=Jua&family=Noto+Sans+KR:wght@700;900&display=swap';
 
 export const HTML2CANVAS_SCRIPT_SRC =
   'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js';
 
 export const FONTS = [
   { name: '검은고딕', value: `'Black Han Sans', sans-serif` },
-  { name: '노토 산스', value: `'Noto Sans KR', sans-serif` },
-  { name: '고딕A1', value: `'Gothic A1', sans-serif` },
-  { name: '도현', value: `'Do Hyeon', sans-serif` },
+  { name: '나눔스퀘어라운드', value: `'NanumSquareRound', sans-serif` },
+  { name: 'GmarketSans', value: `'GmarketSans', sans-serif` },
+  { name: '카페24써라운드', value: `'Cafe24Ssurround', sans-serif` },
+  { name: '에스코어드림', value: `'S-CoreDream', sans-serif` },
   { name: '주아', value: `'Jua', sans-serif` },
-  { name: '구기', value: `'Gugi', cursive` },
-  { name: '싱글데이', value: `'Single Day', cursive` },
-  { name: '나눔 고딕', value: `'Nanum Gothic', sans-serif` },
-  { name: '선플라워', value: `'Sunflower', sans-serif` },
+  { name: '도현', value: `'Do Hyeon', sans-serif` },
+  { name: '노토 산스', value: `'Noto Sans KR', sans-serif` },
 ] as const;
 
 export const SITE_URL = 'https://www.blogsumnail.com';
