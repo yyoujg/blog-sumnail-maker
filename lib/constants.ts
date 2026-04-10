fdd87deb6638b89fe5f00ac91721635094998929
yyoujg <andn1026@gmail.com>
2026-04-10 18:39:07 +0900

    폰트 목록 가독성 위주로 재구성

--- lib/constants.ts
+++ lib/constants.ts
@@ -7,19 +7,19 @@ export const ADSENSE_SCRIPT_SRC = `https://pagead2.googlesyndication.com/pagead/
 export const ADSENSE_AD_SLOT = '9150939859';
 
 export const FONT_LINK_HREF =
-  'https://fonts.googleapis.com/css2?family=Black+Han+Sans&family=Do+Hyeon&family=Gaegu&family=Jua&family=Nanum+Gothic:wght@400;700&family=Nanum+Myeongjo:wght@400;700&family=Noto+Sans+KR:wght@400;700;900&display=swap';
+  'https://fonts.googleapis.com/css2?family=Black+Han+Sans&family=Do+Hyeon&family=Gothic+A1:wght@700;900&family=Jua&family=Nanum+Gothic:wght@700&family=Noto+Sans+KR:wght@700;900&family=Sunflower:wght@700&display=swap';
 
 export const HTML2CANVAS_SCRIPT_SRC =
   'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js';
 
 export const FONTS = [
-  { name: '노토 산스', value: `'Noto Sans KR', sans-serif` },
   { name: '검은고딕', value: `'Black Han Sans', sans-serif` },
+  { name: '노토 산스', value: `'Noto Sans KR', sans-serif` },
+  { name: '고딕A1', value: `'Gothic A1', sans-serif` },
   { name: '도현', value: `'Do Hyeon', sans-serif` },
-  { name: '나눔 고딕', value: `'Nanum Gothic', sans-serif` },
-  { name: '나눔 명조', value: `'Nanum Myeongjo', serif` },
   { name: '주아', value: `'Jua', sans-serif` },
-  { name: '개구', value: `'Gaegu', cursive` },
+  { name: '나눔 고딕', value: `'Nanum Gothic', sans-serif` },
+  { name: '선플라워', value: `'Sunflower', sans-serif` },
 ] as const;
 
 export const SITE_URL = 'https://www.blogsumnail.com';
